ba28c2f04a2e617528746572130aa23df54c45cf
LucasPereira762xi <pereira762xi@gmail.com>
2019-10-14 20:53:53 -0300

    Alters

---
+++ client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,20 @@
+class NegociacaoController {
+
+
+    constructor(){
+
+        let $               = document.querySelector.bind(document);
+
+        this.inputData       = $('#data');
+        this.inputQuantidade = $('#quantidade');
+        this.inputValor      = $('#valor');
+    }
+
+
+    adiciona(event){
+        event.preventDefault();
+
+        
+    }
+
+}
